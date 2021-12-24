da59ceb62621535152824808763cc901cf7c4431
td510510 <dangducthinhdang@gmail.com>
2021-12-24 21:26:42 +0700

    fix padding new-box

--- js/main.js
+++ js/main.js
@@ -111,3 +111,15 @@ videoContainerEl.addEventListener("click", (e) => {
   titleEl.innerHTML = title;
   videoMainEl.play();
 });
+
+// ************
+//  TIME NOW//
+// ************
+const timeNowEl = document.querySelector(".timenow");
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, "0");
+let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+let yyyy = today.getFullYear();
+
+today = dd + "/" + mm + "/" + yyyy;
+timeNowEl.textContent = today;
